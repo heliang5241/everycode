5949c483a9ed79f446e7198932c85f44ed541869
heliang5241 <1960050004@qq.com>
2020-01-18 15:28:42 +0800

    123

--- get_head.py
+++ get_head.py
@@ -3,8 +3,9 @@ import pymysql
 import datetime
 def get_html():
     timestr = str(datetime.datetime.now()).split('.')[0]
-    url = 'https://www.qqak89.com/enter/pc.html'
-    # url = 'https://www.qqct88.com'
+    url = select_mysql()
+    if not url:
+        url = 'https://www.qqak89.com/enter/pc.html'
     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
     headers = {'User-Agent': user_agent}
     try:
@@ -13,11 +14,11 @@ def get_html():
         if http_code == 301:
             headers_info = r.headers
             new_url = headers_info['Location']
-            print(new_url)
+            oper_mysql(new_url,timestr)
     except Exception as e:
         print(e.args)
 
-def oper_mysql():
+def oper_mysql(url,time):
     connect = pymysql.Connect(
         host='localhost',
         port=3306,
@@ -27,8 +28,34 @@ def oper_mysql():
         charset='utf8'
     )
     cursor = connect.cursor()
-    sql = ''
+    sql = "insert into url_info(url,create_time) values('{0}','{1}')".format(url,time)
     cursor.execute(sql)
-
+    connect.commit()
+    cursor.close()
+    connect.close()
+def select_mysql():
+    new_url = ''
+    connect = pymysql.Connect(
+        host='localhost',
+        port=3306,
+        user='onemore',
+        passwd='onemore',
+        db='day1',
+        charset='utf8'
+    )
+    cursor = connect.cursor()
+    sql = "select url from url_info order by id DESC limit 1"
+    cursor.execute(sql)
+    try:
+        for i in cursor.fetchone():
+            new_url = i
+    except Exception as e:
+        print(e.args)
+    cursor.close()
+    connect.close()
+    if new_url:
+        return new_url
+    else:
+        return None
 if __name__ == '__main__':
     get_html()
